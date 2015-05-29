780ccc46797be5e37843b933089e5b5a61ea911f
Vincent Bernardoff <vb@luminar.eu.org>
2015-05-29 12:24:55 +0200

    Remove modules.

--- lib/mt.ml
+++ lib/mt.ml
@@ -1,49 +1,45 @@
-module Trade = struct
-  class ['ts] timestamp ts =
-    object
-      method ts : 'ts = ts
-    end
+class ['ts] timestamp ts =
+  object
+    method ts : 'ts = ts
+  end
 
-  class direction d =
-    object
-      method d : [`Unset | `Bid | `Ask] = d
-    end
+class direction d =
+  object
+    method d : [`Unset | `Bid | `Ask] = d
+  end
 
-  class ['p] tick ~p ~v =
-    object
-      method p : 'p = p
-      method v : 'p = v
-    end
+class ['p] tick ~p ~v =
+  object
+    method p : 'p = p
+    method v : 'p = v
+  end
 
-  class ['p] tick_with_direction ~p ~v ~d =
-    object
-      inherit direction d
-      inherit ['p] tick ~p ~v
-    end
+class ['p] tick_with_direction ~p ~v ~d =
+  object
+    inherit direction d
+    inherit ['p] tick ~p ~v
+  end
 
-  class ['p, 'ts] tick_with_timestamp ~p ~v ~ts =
-    object
-      inherit ['ts] timestamp ts
-      inherit ['p] tick ~p ~v
-    end
+class ['p, 'ts] tick_with_timestamp ~p ~v ~ts =
+  object
+    inherit ['ts] timestamp ts
+    inherit ['p] tick ~p ~v
+  end
 
-  class ['p, 'ts] tick_with_direction_ts ~p ~v ~d ~ts =
-    object
-      inherit direction d
-      inherit ['ts] timestamp ts
-      inherit ['p] tick ~p ~v
-    end
-end
+class ['p, 'ts] tick_with_direction_ts ~p ~v ~d ~ts =
+  object
+    inherit direction d
+    inherit ['ts] timestamp ts
+    inherit ['p] tick ~p ~v
+  end
 
-module Ticker = struct
-  class ['ts, 'p] ticker ~last ~bid ~ask ~high ~low ~volume ~ts =
-    object
-      method last : 'p = last
-      method bid : 'p = bid
-      method ask : 'p = ask
-      method high : 'p = high
-      method low : 'p = low
-      method volume : 'p = volume
-      method timestamp : 'ts = ts
-    end
-end
+class ['ts, 'p] ticker ~last ~bid ~ask ~high ~low ~volume ~ts =
+  object
+    method last : 'p = last
+    method bid : 'p = bid
+    method ask : 'p = ask
+    method high : 'p = high
+    method low : 'p = low
+    method volume : 'p = volume
+    method timestamp : 'ts = ts
+  end
--- lib/mt.mli
+++ lib/mt.mli
@@ -1,52 +1,40 @@
-module Trade :
-  sig
-    class ['ts] timestamp : 'ts -> object method ts : 'ts end
-    class direction :
-      [ `Ask | `Bid | `Unset ] ->
-      object method d : [ `Ask | `Bid | `Unset ] end
-    class ['p] tick : p:'p -> v:'p -> object method p : 'p method v : 'p end
-    class ['p] tick_with_direction :
-      p:'p ->
-      v:'p ->
-      d:[ `Ask | `Bid | `Unset ] ->
-      object
-        method d : [ `Ask | `Bid | `Unset ]
-        method p : 'p
-        method v : 'p
-      end
-    class ['p, 'ts] tick_with_timestamp :
-      p:'p ->
-      v:'p ->
-      ts:'ts -> object method p : 'p method ts : 'ts method v : 'p end
-    class ['p, 'ts] tick_with_direction_ts :
-      p:'p ->
-      v:'p ->
-      d:[ `Ask | `Bid | `Unset ] ->
-      ts:'ts ->
-      object
-        method d : [ `Ask | `Bid | `Unset ]
-        method p : 'p
-        method ts : 'ts
-        method v : 'p
-      end
+class ['ts] timestamp : 'ts -> object method ts : 'ts end
+class direction :
+  [ `Ask | `Bid | `Unset ] -> object method d : [ `Ask | `Bid | `Unset ] end
+class ['p] tick : p:'p -> v:'p -> object method p : 'p method v : 'p end
+class ['p] tick_with_direction :
+  p:'p ->
+  v:'p ->
+  d:[ `Ask | `Bid | `Unset ] ->
+  object method d : [ `Ask | `Bid | `Unset ] method p : 'p method v : 'p end
+class ['p, 'ts] tick_with_timestamp :
+  p:'p ->
+  v:'p -> ts:'ts -> object method p : 'p method ts : 'ts method v : 'p end
+class ['p, 'ts] tick_with_direction_ts :
+  p:'p ->
+  v:'p ->
+  d:[ `Ask | `Bid | `Unset ] ->
+  ts:'ts ->
+  object
+    method d : [ `Ask | `Bid | `Unset ]
+    method p : 'p
+    method ts : 'ts
+    method v : 'p
   end
-module Ticker :
-  sig
-    class ['ts, 'p] ticker :
-      last:'p ->
-      bid:'p ->
-      ask:'p ->
-      high:'p ->
-      low:'p ->
-      volume:'p ->
-      ts:'ts ->
-      object
-        method ask : 'p
-        method bid : 'p
-        method high : 'p
-        method last : 'p
-        method low : 'p
-        method timestamp : 'ts
-        method volume : 'p
-      end
+class ['ts, 'p] ticker :
+  last:'p ->
+  bid:'p ->
+  ask:'p ->
+  high:'p ->
+  low:'p ->
+  volume:'p ->
+  ts:'ts ->
+  object
+    method ask : 'p
+    method bid : 'p
+    method high : 'p
+    method last : 'p
+    method low : 'p
+    method timestamp : 'ts
+    method volume : 'p
   end
