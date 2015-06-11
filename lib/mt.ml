b9cae3a9e1162a865524e58890368dae9eb6975e
Vincent Bernardoff <vb@luminar.eu.org>
2015-06-11 09:27:10 +0200

    More structure / factorisation.

--- lib/mt.ml
+++ lib/mt.ml
@@ -4,133 +4,171 @@ module Int64 = struct
   let ( - ) = sub
 end
 
-class ['ts] timestamp ts =
-  object
-    method ts : 'ts = ts
+module Timestamp = struct
+  class ['ts] t ts =
+    object
+      method ts : 'ts = ts
+    end
+
+  class ['ts] tns ts ns =
+    object
+      inherit ['ts] t ts
+      method ns : 'ts = ns
+    end
+end
+
+module Direction = struct
+  type t = [`Unset | `Bid | `Ask] [@@deriving show,enum]
+  let of_enum_exn d = match of_enum d with
+    | Some d -> d
+    | None -> invalid_arg "d_of_enum"
+
+  class d d =
+    object
+      method d : t = d
+    end
+end
+
+module Tick = struct
+  class ['p] t ~p ~v =
+    object
+      method p : 'p = p
+      method v : 'p = v
+    end
+
+  class ['p] td ~p ~v ~d =
+    object
+      inherit Direction.d d
+      inherit ['p] t p v
+    end
+
+  class ['p, 'ts] tts ~ts ~p ~v =
+    object
+      inherit ['ts] Timestamp.t ts
+      inherit ['p] t p v
+    end
+
+  class ['p, 'ts] tdts ~ts ~p ~v ~d =
+    object
+      inherit Direction.d d
+      inherit ['ts] Timestamp.t ts
+      inherit ['p] t p v
+    end
+
+  class ['p, 'ts] tdtsns ~ts ~ns ~p ~v ~d =
+    object
+      inherit Direction.d d
+      inherit ['ts] Timestamp.tns ts ns
+      inherit ['p] t p v
+    end
+
+  let show_tdts o =
+    let ts = Int64.(o#ts div 1_000_000_000L) in
+    let ns = Int64.(o#ts rem 1_000_000_000L) in
+    Format.sprintf "< ts = %Ld.%Ld, p = %Ld, v = %Ld, d = %s >"
+      ts ns o#p o#v (Direction.show o#d)
+
+  let pp_tdts fmt o =
+    let ts = Int64.(o#ts div 1_000_000_000L) in
+    let ns = Int64.(o#ts rem 1_000_000_000L) in
+    Format.fprintf fmt "< ts = %Ld.%Ld, p = %Ld, v = %Ld, d = %a >"
+      ts ns o#p o#v Direction.pp o#d
+
+
+  let show_tdtsns o =
+    Format.sprintf "< ts = %Ld, ns = %Ld, p = %Ld, v = %Ld, d = %s >"
+      o#ts o#ns o#p o#v (Direction.show o#d)
+
+  let pp_tdtsns fmt o =
+    Format.fprintf fmt "< ts = %Ld, ns = %Ld, p = %Ld, v = %Ld, d = %a >"
+      o#ts o#ns o#p o#v Direction.pp o#d
+
+  let int64_of_v_d v d =
+    let open Int64 in
+    logor
+      (logand v (shift_left 1L 62 - 1L))
+      (shift_left (of_int (Direction.to_enum d)) 62)
+
+  let v_d_of_int64 i =
+    let open Int64 in
+    let d = shift_right_logical i 62 |> to_int |> Direction.of_enum_exn in
+    let v = logand i (shift_left 1L 62 - 1L) in
+    v, d
+
+  module type IO = sig
+    type t
+    val length : t -> int
+    val get_int64 : t -> int -> int64
+    val set_int64 : t -> int -> int64 -> unit
   end
 
-class ['ts] timestamp_ns ts ns =
-  object
-    inherit ['ts] timestamp ts
-    method ns : 'ts = ns
+  module TickIO (IO: IO) = struct
+    open IO
+    let write_dtsns b off o =
+      set_int64 b off o#ts;
+      set_int64 b (off+8) o#ns;
+      set_int64 b (off+16) o#p;
+      set_int64 b (off+24) @@ int64_of_v_d o#v o#d
+
+    let read_dtsns b off =
+      let ts = get_int64 b off in
+      let ns = get_int64 b (off+8) in
+      let p = get_int64 b (off+16) in
+      let v = get_int64 b (off+24) in
+      let v, d = v_d_of_int64 v in
+      new tdtsns ts ns p v d
+
+    let write_dts b off o =
+      set_int64 b off o#ts;
+      set_int64 b (off+8) o#p;
+      set_int64 b (off+16) @@ int64_of_v_d o#v o#d
+
+    let read_dts b off =
+      let ts = get_int64 b off in
+      let p = get_int64 b (off+8) in
+      let v = get_int64 b (off+16) in
+      let v, d = v_d_of_int64 v in
+      new tdts ts p v d
   end
 
-type d = [`Unset | `Bid | `Ask] [@@deriving show,enum]
-let d_of_enum_exn d = match d_of_enum d with
-  | Some d -> d
-  | None -> invalid_arg "d_of_enum"
-
-class direction d =
-  object
-    method d : d = d
+  module Bytes = struct
+    include Bytes
+    let get_int64 = EndianBytes.BigEndian.get_int64
+    let set_int64 = EndianBytes.BigEndian.set_int64
   end
 
-class ['p] tick ~p ~v =
-  object
-    method p : 'p = p
-    method v : 'p = v
+  module Bigstring = struct
+    include CCBigstring
+    let get_int64 = EndianBigstring.BigEndian.get_int64
+    let set_int64 = EndianBigstring.BigEndian.set_int64
   end
 
-class ['p] tick_with_direction ~p ~v ~d =
-  object
-    inherit direction d
-    inherit ['p] tick p v
-  end
+  module TickBytes = TickIO(Bytes)
+  module TickBigstring = TickIO(Bigstring)
+end
 
-class ['p, 'ts] tick_with_timestamp ~ts ~p ~v =
-  object
-    inherit ['ts] timestamp ts
-    inherit ['p] tick p v
-  end
+module Ticker = struct
+  class ['ts, 'p] t ~last ~bid ~ask ~high ~low ~volume ~ts =
+    object
+      method last : 'p = last
+      method bid : 'p = bid
+      method ask : 'p = ask
+      method high : 'p = high
+      method low : 'p = low
+      method volume : 'p = volume
+      method timestamp : 'ts = ts
+    end
 
-class ['p, 'ts] tick_with_direction_ts ~ts ~p ~v ~d =
-  object
-    inherit direction d
-    inherit ['ts] timestamp ts
-    inherit ['p] tick p v
-  end
+  class ['ts, 'p] tvwap ~vwap ~last ~bid ~ask ~high ~low ~volume ~ts =
+    object
+      inherit ['ts, 'p] t ~last ~bid ~ask ~high ~low ~volume ~ts
+      method vwap : 'p = vwap
+    end
+end
 
-class ['p, 'ts] tick_with_d_ts_ns ~ts ~ns ~p ~v ~d =
-  object
-    inherit direction d
-    inherit ['ts] timestamp_ns ts ns
-    inherit ['p] tick p v
-  end
-
-let show_tick_with_d_ts_ns o =
-  Format.sprintf "< ts = %Ld, ns = %Ld, p = %Ld, v = %Ld, d = %s >"
-    o#ts o#ns o#p o#v (show_d o#d)
-
-let pp_tick_with_d_ts_ns fmt o =
-  Format.fprintf fmt "< ts = %Ld, ns = %Ld, p = %Ld, v = %Ld, d = %a >"
-      o#ts o#ns o#p o#v pp_d o#d
-
-let int64_of_v_d v d =
-  let open Int64 in
-  logor
-    (logand v (shift_left 1L 62 - 1L))
-    (shift_left (of_int (d_to_enum d)) 62)
-
-let v_d_of_int64 i =
-  let open Int64 in
-  let d = shift_right_logical i 62 |> to_int |> d_of_enum_exn in
-  let v = logand i (shift_left 1L 62 - 1L) in
-  v, d
-
-let tick_with_d_ts_ns_to_bytes b off o =
-  let open EndianBytes.BigEndian in
-  set_int64 b off o#ts;
-  set_int64 b (off+8) o#ns;
-  set_int64 b (off+16) o#p;
-  set_int64 b (off+24) @@ int64_of_v_d o#v o#d
-
-let tick_with_d_ts_ns_to_bigstring b off o =
-  let open EndianBigstring.BigEndian in
-  set_int64 b off o#ts;
-  set_int64 b (off+8) o#ns;
-  set_int64 b (off+16) o#p;
-  set_int64 b (off+24) @@ int64_of_v_d o#v o#d
-
-let tick_with_d_ts_ns_of_bytes b off =
-  let open EndianBytes.BigEndian in
-  if Bytes.length b <> 32 then None
-  else
-    let ts = get_int64 b off in
-    let ns = get_int64 b (off+8) in
-    let p = get_int64 b (off+16) in
-    let v = get_int64 b (off+24) in
-    let v, d = v_d_of_int64 v in
-    Some (new tick_with_d_ts_ns ts ns p v d)
-
-let tick_with_d_ts_ns_of_bigstring b off =
-  let open EndianBigstring.BigEndian in
-  if CCBigstring.length b <> 32 then None
-  else
-    let ts = get_int64 b off in
-    let ns = get_int64 b (off+8) in
-    let p = get_int64 b (off+16) in
-    let v = get_int64 b (off+24) in
-    let v, d = v_d_of_int64 v in
-    Some (new tick_with_d_ts_ns ts ns p v d)
-
-class ['ts, 'p] ticker ~last ~bid ~ask ~high ~low ~volume ~ts =
-  object
-    method last : 'p = last
-    method bid : 'p = bid
-    method ask : 'p = ask
-    method high : 'p = high
-    method low : 'p = low
-    method volume : 'p = volume
-    method timestamp : 'ts = ts
-  end
-
-class ['ts, 'p] ticker_with_vwap ~vwap ~last ~bid ~ask ~high ~low ~volume ~ts =
-  object
-    inherit ['ts, 'p] ticker ~last ~bid ~ask ~high ~low ~volume ~ts
-    method vwap : 'p = vwap
-  end
-
-type 'a orderbook = {
-  bids: 'a list;
-  asks: 'a list;
-} [@@deriving show,create]
+module OrderBook = struct
+  type 'a t = {
+    bids: 'a list;
+    asks: 'a list;
+  } [@@deriving show,create]
+end
--- lib/mt.mli
+++ lib/mt.mli
@@ -1,104 +1,183 @@
-class ['ts] timestamp : 'ts -> object method ts : 'ts end
-class ['ts] timestamp_ns :
-  'ts -> 'ts -> object method ns : 'ts method ts : 'ts end
-type d = [ `Ask | `Bid | `Unset ]
-val pp_d : Format.formatter -> [< `Ask | `Bid | `Unset ] -> unit
-val show_d : [< `Ask | `Bid | `Unset ] -> string
-val min_d : int
-val max_d : int
-val d_to_enum : [< `Ask | `Bid | `Unset ] -> int
-val d_of_enum : int -> [> `Ask | `Bid | `Unset ] option
-val d_of_enum_exn : int -> [> `Ask | `Bid | `Unset ]
-class direction : d -> object method d : d end
-class ['p] tick : p:'p -> v:'p -> object method p : 'p method v : 'p end
-class ['p] tick_with_direction :
-  p:'p -> v:'p -> d:d -> object method d : d method p : 'p method v : 'p end
-class ['p, 'ts] tick_with_timestamp :
-  ts:'ts ->
-  p:'p -> v:'p -> object method p : 'p method ts : 'ts method v : 'p end
-class ['p, 'ts] tick_with_direction_ts :
-  ts:'ts ->
-  p:'p ->
-  v:'p ->
-  d:d -> object method d : d method p : 'p method ts : 'ts method v : 'p end
-class ['p, 'ts] tick_with_d_ts_ns :
-  ts:'ts ->
-  ns:'ts ->
-  p:'p ->
-  v:'p ->
-  d:d ->
-  object
-    method d : d
-    method ns : 'ts
-    method p : 'p
-    method ts : 'ts
-    method v : 'p
+module Timestamp :
+  sig
+    class ['ts] t : 'ts -> object method ts : 'ts end
+    class ['ts] tns :
+      'ts -> 'ts -> object method ns : 'ts method ts : 'ts end
   end
-val show_tick_with_d_ts_ns :
-  < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
-    v : int64; .. > ->
-  string
-val pp_tick_with_d_ts_ns :
-  Format.formatter ->
-  < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
-    v : int64; .. > ->
-  unit
-val int64_of_v_d : int64 -> [< `Ask | `Bid | `Unset ] -> int64
-val v_d_of_int64 : int64 -> int64 * [> `Ask | `Bid | `Unset ]
-val tick_with_d_ts_ns_to_bytes :
-  Bytes.t ->
-  int ->
-  < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
-    v : int64; .. > ->
-  unit
-val tick_with_d_ts_ns_to_bigstring :
-  EndianBigstring.bigstring ->
-  int ->
-  < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
-    v : int64; .. > ->
-  unit
-val tick_with_d_ts_ns_of_bytes :
-  Bytes.t -> int -> (int64, int64) tick_with_d_ts_ns option
-val tick_with_d_ts_ns_of_bigstring :
-  CCBigstring.t -> int -> (int64, int64) tick_with_d_ts_ns option
-class ['ts, 'p] ticker :
-  last:'p ->
-  bid:'p ->
-  ask:'p ->
-  high:'p ->
-  low:'p ->
-  volume:'p ->
-  ts:'ts ->
-  object
-    method ask : 'p
-    method bid : 'p
-    method high : 'p
-    method last : 'p
-    method low : 'p
-    method timestamp : 'ts
-    method volume : 'p
+
+module Direction :
+  sig
+    type t = [ `Ask | `Bid | `Unset ]
+    val pp : Format.formatter -> [< `Ask | `Bid | `Unset ] -> unit
+    val show : [< `Ask | `Bid | `Unset ] -> string
+    val min : int
+    val max : int
+    val to_enum : [< `Ask | `Bid | `Unset ] -> int
+    val of_enum : int -> [> `Ask | `Bid | `Unset ] option
+    val of_enum_exn : int -> [> `Ask | `Bid | `Unset ]
+    class d : t -> object method d : t end
   end
-class ['ts, 'p] ticker_with_vwap :
-  vwap:'p ->
-  last:'p ->
-  bid:'p ->
-  ask:'p ->
-  high:'p ->
-  low:'p ->
-  volume:'p ->
-  ts:'ts ->
-  object
-    method ask : 'p
-    method bid : 'p
-    method high : 'p
-    method last : 'p
-    method low : 'p
-    method timestamp : 'ts
-    method volume : 'p
-    method vwap : 'p
+
+module Tick :
+  sig
+    class ['p] t : p:'p -> v:'p -> object method p : 'p method v : 'p end
+    class ['p] td :
+      p:'p ->
+      v:'p ->
+      d:Direction.t ->
+      object method d : Direction.t method p : 'p method v : 'p end
+    class ['p, 'ts] tts :
+      ts:'ts ->
+      p:'p -> v:'p -> object method p : 'p method ts : 'ts method v : 'p end
+    class ['p, 'ts] tdts :
+      ts:'ts ->
+      p:'p ->
+      v:'p ->
+      d:Direction.t ->
+      object
+        method d : Direction.t
+        method p : 'p
+        method ts : 'ts
+        method v : 'p
+      end
+    class ['p, 'ts] tdtsns :
+      ts:'ts ->
+      ns:'ts ->
+      p:'p ->
+      v:'p ->
+      d:Direction.t ->
+      object
+        method d : Direction.t
+        method ns : 'ts
+        method p : 'p
+        method ts : 'ts
+        method v : 'p
+      end
+    val show_tdts :
+      < d : [< `Ask | `Bid | `Unset ]; p : int64;
+        ts : (int64 -> int64 -> int64) -> int64 -> int64; v : int64; .. > ->
+      string
+    val pp_tdts :
+      Format.formatter ->
+      < d : [< `Ask | `Bid | `Unset ]; p : int64;
+        ts : (int64 -> int64 -> int64) -> int64 -> int64; v : int64; .. > ->
+      unit
+    val show_tdtsns :
+      < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
+        v : int64; .. > ->
+      string
+    val pp_tdtsns :
+      Format.formatter ->
+      < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : int64;
+        v : int64; .. > ->
+      unit
+    val int64_of_v_d : int64 -> [< `Ask | `Bid | `Unset ] -> int64
+    val v_d_of_int64 : int64 -> int64 * [> `Ask | `Bid | `Unset ]
+    module type IO =
+      sig
+        type t
+        val length : t -> int
+        val get_int64 : t -> int -> int64
+        val set_int64 : t -> int -> int64 -> unit
+      end
+    module TickIO :
+      functor (IO : IO) ->
+        sig
+          val write_dtsns :
+            IO.t ->
+            int ->
+            < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64;
+              ts : int64; v : int64; .. > ->
+            unit
+          val read_dtsns : IO.t -> int -> (int64, int64) tdtsns
+          val write_dts :
+            IO.t ->
+            int ->
+            < d : [< `Ask | `Bid | `Unset ]; p : int64; ts : int64;
+              v : int64; .. > ->
+            unit
+          val read_dts : IO.t -> int -> (int64, int64) tdts
+        end
+
+    module TickBytes :
+      sig
+        val write_dtsns :
+          Bytes.t ->
+          int ->
+          < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : 
+            int64; v : int64; .. > ->
+          unit
+        val read_dtsns : Bytes.t -> int -> (int64, int64) tdtsns
+        val write_dts :
+          Bytes.t ->
+          int ->
+          < d : [< `Ask | `Bid | `Unset ]; p : int64; ts : int64; v : 
+            int64; .. > ->
+          unit
+        val read_dts : Bytes.t -> int -> (int64, int64) tdts
+      end
+    module TickBigstring :
+      sig
+        val write_dtsns :
+          CCBigstring.t ->
+          int ->
+          < d : [< `Ask | `Bid | `Unset ]; ns : int64; p : int64; ts : 
+            int64; v : int64; .. > ->
+          unit
+        val read_dtsns : CCBigstring.t -> int -> (int64, int64) tdtsns
+        val write_dts :
+          CCBigstring.t ->
+          int ->
+          < d : [< `Ask | `Bid | `Unset ]; p : int64; ts : int64; v : 
+            int64; .. > ->
+          unit
+        val read_dts : CCBigstring.t -> int -> (int64, int64) tdts
+      end
   end
-type 'a orderbook = { bids : 'a list; asks : 'a list; }
-val pp_orderbook :
-  (Format.formatter -> 'a -> 'b) -> Format.formatter -> 'a orderbook -> unit
-val show_orderbook : (Format.formatter -> 'a -> 'b) -> 'a orderbook -> string
-val create_orderbook : ?bids:'a list -> ?asks:'a list -> unit -> 'a orderbook
+module Ticker :
+  sig
+    class ['ts, 'p] t :
+      last:'p ->
+      bid:'p ->
+      ask:'p ->
+      high:'p ->
+      low:'p ->
+      volume:'p ->
+      ts:'ts ->
+      object
+        method ask : 'p
+        method bid : 'p
+        method high : 'p
+        method last : 'p
+        method low : 'p
+        method timestamp : 'ts
+        method volume : 'p
+      end
+    class ['ts, 'p] tvwap :
+      vwap:'p ->
+      last:'p ->
+      bid:'p ->
+      ask:'p ->
+      high:'p ->
+      low:'p ->
+      volume:'p ->
+      ts:'ts ->
+      object
+        method ask : 'p
+        method bid : 'p
+        method high : 'p
+        method last : 'p
+        method low : 'p
+        method timestamp : 'ts
+        method volume : 'p
+        method vwap : 'p
+      end
+  end
+module OrderBook :
+  sig
+    type 'a t = { bids : 'a list; asks : 'a list; }
+    val pp :
+      (Format.formatter -> 'a -> 'b) -> Format.formatter -> 'a t -> unit
+    val show : (Format.formatter -> 'a -> 'b) -> 'a t -> string
+    val create : ?bids:'a list -> ?asks:'a list -> unit -> 'a t
+  end
